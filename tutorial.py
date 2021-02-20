a49c35c6e518b93a0ff782c6d1fec6637799fb7a
Hripsime K <k.ripsime@hotmail.com>
2021-02-20 18:20:36 +0100

    remove code

--- tutorial.py
+++ tutorial.py
@@ -1,16 +1,1 @@
 print ("hello")
-
-print("cheeseburger")
-print("cheeseburger")
-print("cheeseburger")
-print("cheese")
-print("cheeseburger")
-print("cheeseburger")
-
-t = 10
-
-
-print ("pizzaburger")
-
-
-print("ja")
